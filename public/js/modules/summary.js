dd00246331fd6bbd80e1f05a4991d070f89f8765
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-26 17:04:39 +0000

    npm install

--- public/js/modules/summary.js
+++
@@ -1,8 +0,0 @@
-export function setupSummary() {
-  const summaryInput = document.getElementById('summary');
-  
-  summaryInput.addEventListener('input', () => {
-    document.getElementById('preview-summary').textContent = 
-      summaryInput.value || 'Your professional summary will appear here.';
-  });
-} 
